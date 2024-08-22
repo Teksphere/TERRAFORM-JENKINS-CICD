e41c06cceea8259afc8f9de18032c6f2e29ff8ee
Teksphere <160017075+Teksphere@users.noreply.github.com>
2024-08-22 16:09:38 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "mybucket" {
-  bucket = var.bucket_name
+  bucket = var.bucketname
 }
 resource "aws_s3_bucket_ownership_controls" "example" {
   bucket = aws_s3_bucket.mybucket.id
