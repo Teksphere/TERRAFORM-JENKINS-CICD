33c99db7c230879a69e1d0ca74b42be83be3a11e
Teksphere <160017075+Teksphere@users.noreply.github.com>
2024-08-22 15:19:35 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "ami_id" {
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "teksphere-777-ajaykumar-yegireddi"
+  default     = "teksphere-777-cloud"
 }
 
 variable "acl" {
